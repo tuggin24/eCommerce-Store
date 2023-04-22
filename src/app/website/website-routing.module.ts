736227e59dae97b25adef42ee2dd86a1f18711f4
tuggin24 <agustinloko74@gmail.com>
2023-04-22 18:30:17 -0500

    cambios deploy firebase

--- src/app/website/website-routing.module.ts
+++ src/app/website/website-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
     path: '', component: LayoutComponent,
     children: [
       {
-        path: '', redirectTo: 'home', pathMatch: 'full'
+        path: '', redirectTo: '/home', pathMatch: 'full'
       },
       {
         path: 'home', component: HomeComponent
